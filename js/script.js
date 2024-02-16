04d6fa9ae88e64f15a4a8b9b404112a6616d04e6
Md Ataullah <ataullahm100@gmail.com>
2024-02-16 21:00:19 +0600

    card click and added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log('Hello');
+console.log('Hello Js');
+const cards = document.getElementsByClassName('card');
+for (const card of cards) {
+  card.addEventListener('click', function () {
+    const cardNam = card.querySelector('h2').innerText;
+    const cardNameCreate = document.createElement('p');
+    cardNameCreate.innerText = cardNam;
+    const cardList = document.getElementById('productItem');
+    cardList.appendChild(cardNameCreate);
+  });
+}
