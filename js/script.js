75ffee0863645df09e6c23dacac9ea9e11f2a77a
Md Ataullah <ataullahm100@gmail.com>
2024-02-16 23:26:44 +0600

    pricing adde

--- js/script.js
+++ js/script.js
@@ -17,6 +17,25 @@ for (const card of cards) {
     priceCount += price;
     const totalPrice = document.getElementById('totalPrice');
     totalPrice.innerText = priceCount.toFixed(2);
-    // console.log(totalPrice);
+    const total = document.getElementById('total');
+    total.innerText = priceCount.toFixed(2);
+
+    document.getElementById('btnCoupon').addEventListener('click', function () {
+      const discountPrice = document.getElementById('discountPrice');
+      const total = document.getElementById('total');
+      const coupon = document.getElementById('couponId').value;
+      const couponCode = coupon.toLowerCase();
+      if (priceCount >= 3000 && couponCode === 'ats20') {
+        const discount = (priceCount * 20) / 100;
+        discountPrice.innerText = discount.toFixed(2);
+        const totalP = priceCount - discount;
+        total.innerText = totalP.toFixed(2);
+      }
+    });
+
+    document.getElementById('purchase').addEventListener('click', function () {
+      const mass = document.getElementById('congratulateMassage');
+      mass.classList.remove('hidden');
+    });
   });
 }
