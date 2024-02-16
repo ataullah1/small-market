8f26ae0f027dfc9e5a71bb16468dbe39ef2b37a3
Md Ataullah <ataullahm100@gmail.com>
2024-02-16 21:36:38 +0600

    card prices added

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
 console.log('Hello Js');
+let count = 1;
+let priceCount = 0;
 const cards = document.getElementsByClassName('card');
 for (const card of cards) {
   card.addEventListener('click', function () {
     const cardNam = card.querySelector('h2').innerText;
     const cardNameCreate = document.createElement('p');
-    cardNameCreate.innerText = cardNam;
+    cardNameCreate.innerText = count + '. ' + cardNam;
     const cardList = document.getElementById('productItem');
     cardList.appendChild(cardNameCreate);
+    count++;
+
+    // prices count
+    const prices = card.querySelector('span');
+    const price = parseFloat(prices.innerText);
+    priceCount += price;
+    const totalPrice = document.getElementById('totalPrice');
+    totalPrice.innerText = priceCount.toFixed(2);
+    // console.log(totalPrice);
   });
 }
